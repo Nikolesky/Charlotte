44dda1127260882fb981c1dca8e046b0d77c6388
Nikolesky <vempatinityan@gmail.com>
2025-06-29 22:29:56 +0530

    made basic structure of chat engine

---
+++ chat_engine.py
@@ -0,0 +1,24 @@
+
+def start_chat():
+    from google import genai
+    from google.genai import types
+    from config import GENAI_API_KEY
+
+    client = genai.Client(api_key=GENAI_API_KEY)
+
+    chat = client.chats.create(model='gemini-2.0-flash')
+
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            break
+
+        response = client.models.generate_content_stream(
+            model='gemini-2.0-flash',
+            config=types.GenerateContentConfig(
+            system_instruction="You are Nikolesky's personalized AI assistant, Charlotte. You are helpful, creative, and friendly.",),
+            contents=user_input
+        )
+        print("Charlotte: ", end='')
+        for stream in response:
+            print(stream.text)
--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-from google import genai
-from google.genai import types
-from config import GENAI_API_KEY
+from chat_engine import start_chat
 
-client = genai.Client(api_key=GENAI_API_KEY)
-
-chat = client.chats.create(model='gemini-2.0-flash')
-
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == 'exit':
-        break
-
-    response = client.models.generate_content_stream(
-        model='gemini-2.0-flash',
-        config=types.GenerateContentConfig(
-        system_instruction="You are Nikolesky's personalized AI assistant, Charlotte. You are helpful, creative, and friendly.",),
-        contents=user_input
-        )
-    print("Charlotte: ", end='')
-    for stream in response:
-        print(stream.text)
+start_chat()
