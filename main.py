2d638fbc1215235c559b2b213557da959e3b2b8d
Nikolesky <vempatinityan@gmail.com>
2025-06-29 14:27:35 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from google import genai
+from google.genai import types
 from config import GENAI_API_KEY
 
 client = genai.Client(api_key=GENAI_API_KEY)
 
-chat = client.chats.create(model='gemini-2.0-flash',)
+chat = client.chats.create(model='gemini-2.0-flash')
 
 while True:
     user_input = input("You: ")
     if user_input.lower() == 'exit':
         break
 
-    response = chat.send_message(user_input)
-    print(f"AI: {response.text}")
+    response = client.models.generate_content_stream(
+        model='gemini-2.0-flash',
+        config=types.GenerateContentConfig(
+        system_instruction="You are Nikolesky's personalized AI assistant, Charlotte. You are helpful, creative, and friendly.",),
+        contents=user_input
+        )
+    print("Charlotte: ", end='')
+    for stream in response:
+        print(stream.text)
